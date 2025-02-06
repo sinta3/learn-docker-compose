f5bf48b77ada290b3aec385ec463072605a8a7d8
sinta3 <sinta.tri38@gmail.com>
2025-02-06 22:04:23 +0700

    learn: start container, comment, until list project name with docker compose config

---
+++ script.sh
@@ -0,0 +1,19 @@
+#DOCKER COMPOSE INSTRUCTION
+
+#CREATE CONTAINER
+docker compose create #enter directory when docker compose file placed
+
+#START CONTAINER
+docker compose start
+
+#LISTING ALL CONTAINER that exists from docker compose
+docker compose ps
+
+#STOP CONTAINER
+docker compose stop
+
+#REMOVE CONTAINER
+docker compose down
+
+#LISTING ALL PROJECT NAME that exists from docker compose
+docker compose ls
